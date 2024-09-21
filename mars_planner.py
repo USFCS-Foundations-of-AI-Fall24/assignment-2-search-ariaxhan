a028837b8896d82e07e79b135dfcca61bda21d3d
Aria Han <ariaxhan@gmail.com>
2024-09-20 19:46:53 -0700

    working on mars planner

--- mars_planner.py
+++ mars_planner.py
@@ -15,6 +15,7 @@
 
 from copy import deepcopy
 from search_algorithms import breadth_first_search
+from search_algorithms import depth_first_search
 
 class RoverState :
     def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False):
@@ -26,7 +27,10 @@ class RoverState :
 
     ## you do this.
     def __eq__(self, other):
-       pass
+       return (self.loc == other.loc and
+                self.sample_extracted == other.sample_extracted and
+                self.holding_sample == other.holding_sample and
+                self.charged == other.charged)
 
 
     def __repr__(self):
@@ -61,7 +65,6 @@ def move_to_station(state) :
     r2.loc = "station"
     r2.prev = state
     return r2
-
 def move_to_battery(state) :
     r2 = deepcopy(state)
     r2.loc = "battery"
@@ -99,14 +102,19 @@ def battery_goal(state) :
     return state.loc == "battery"
 ## add your goals here.
 
-def mission_complete(state) :
-    pass
+def sample_goal(state) :
+    return state.holding_sample
 
+def charged_goal(state) :
+    return state.charged
+
+def mission_complete(state) :
+    # returns True if we are at the battery, charged, and the sample is at the station.
+    return (battery_goal(state) and charged_goal == True and sample_goal(state) == False)
 
 if __name__=="__main__" :
     s = RoverState()
     result = breadth_first_search(s, action_list, mission_complete)
-    print(result)
-
-
-
+    print("Breadth first search result", result)
+    result2 = depth_first_search(s, action_list, mission_complete)
+    print("Depth first search result ", result2)
