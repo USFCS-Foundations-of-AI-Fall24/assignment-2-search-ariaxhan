ab01664c11b5a2a315b5324b72bb3c75dfb10bee
Aria Han <ariaxhan@gmail.com>
2024-09-21 17:02:40 -0700

    implementing depth limited search

--- search_algorithms.py
+++ search_algorithms.py
@@ -41,14 +41,15 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
 def depth_first_search(startState, action_list, goal_test, use_closed_list=True,limit=0) :
     search_queue = deque()
     closed_list = {}
-
-    search_queue.append((startState,""))
+    search_queue.append((startState,"", 0))
     if use_closed_list :
         closed_list[startState] = True
     states_generated = 1  # Count the start state
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
         next_state = search_queue.pop()
+        if next_state[2] >= limit:  # Skip states that have reached the depth limit
+            continue
         if goal_test(next_state[0]):
             print("Goal found")
             print(next_state)
@@ -59,13 +60,12 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
             print(f"DFS generated {states_generated} states.")
             return next_state
         else :
-            successors = next_state[0].successors(action_list)
+            successors = [(s[0], s[1], next_state[2] + 1) for s in next_state[0].successors(action_list)]
             states_generated += len(successors)
             if use_closed_list :
                 successors = [item for item in successors
                                     if item[0] not in closed_list]
+
                 for s in successors :
                     closed_list[s[0]] = True
             search_queue.extend(successors)
-
-## add iterative deepening search here
